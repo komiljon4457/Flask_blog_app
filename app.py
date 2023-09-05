a3e7c9a2c883a496e3f30bb3c0fd0a2eb842fe18
Komiljon Yangiboev <k.yangiboev@baik.uz>
2023-09-05 12:26:15 +0500

    add files v2

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def redirect():
     return "This is another page of the web app"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
