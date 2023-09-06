bb0649887a1acb1f39910ed422322264cff26804
Komiljon Yangiboev <k.yangiboev@baik.uz>
2023-09-06 09:24:48 +0500

    add changes

--- user_data_script.sh
+++ user_data_script.sh
@@ -4,7 +4,7 @@ echo "Docker is not installed. Installing..."
 apt update && apt upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo docker pull integrity11/my_blog_app:latest
-sudo docker run --rm integrity11/my_blog_app:latest
+sudo docker pull integrity11/my_blog_app:v1
+sudo docker run --rm integrity11/my_blog_app:v1
 
 
