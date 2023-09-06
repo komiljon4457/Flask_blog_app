ce6414305a396d3b809a503ebfbb2b23897207a6
Komiljon Yangiboev <k.yangiboev@baik.uz>
2023-09-06 09:28:23 +0500

    add chnages

--- user_data_script.sh
+++ user_data_script.sh
@@ -5,6 +5,6 @@ apt update && apt upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo docker pull integrity11/my_blog_app:v1
-sudo docker run --rm integrity11/my_blog_app:v1
+sudo docker run -p 80:80  integrity11/my_blog_app:v1
 
 
