e02edc882dbb8c5f3dcaa311e1caba004e3519ac
Komiljon Yangiboev <k.yangiboev@baik.uz>
2023-09-07 12:59:01 +0500

    add changes

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,9 +1,9 @@
-#cloud-boothook
 #!/bin/bash
 echo "Docker is not installed. Installing..."
-apt update && apt upgrade
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+yum update -y
+yum install docker
+service docker start
+usermod -a -G docker ec2-user
 sudo docker run -d -p 80:80  integrity11/my_flask_app:v3
 
 
