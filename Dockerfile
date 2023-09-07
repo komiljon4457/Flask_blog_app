13f54e44f3b25862a85f39ad21864c5f5d158980
Komiljon Yangiboev <k.yangiboev@baik.uz>
2023-09-07 12:52:33 +0500

    add changes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ WORKDIR /app
 COPY ./requirements.txt /app
 RUN pip install -r requirements.txt
 COPY . .
-EXPOSE 80
-ENV FLASK_APP=app.py
-ENTRYPOINT ["flask", "run"]
+ENTRYPOINT python app.py
--- user_data_script.sh
+++ user_data_script.sh
@@ -4,7 +4,6 @@ echo "Docker is not installed. Installing..."
 apt update && apt upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo docker pull integrity11/my_blog_app:v1
-sudo docker run -p 80:80  integrity11/my_blog_app:v1
+sudo docker run -d -p 80:80  integrity11/my_flask_app:v3
 
 
