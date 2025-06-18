5a03e324d072e5dc4b7be61b28df0b32f03c0893
Komiljon Yangiboev <k.yangiboev@baik.uz>
2025-06-18 23:46:35 +0500

    make changes some

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Docker is not installed. Installing..."
 sudo yum update -y # file update
-sudo yum install docker
+sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo docker run -d -p 80:5000  integrity11/blog_app_v2:latest
